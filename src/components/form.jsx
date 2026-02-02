37c35b1f2dcb4034da0974f523d3d3fd20f33198
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-02-02 18:21:10 +0100

    add test forms

---
+++ src/components/form.jsx
@@ -0,0 +1,46 @@
+import React, {useRef, useState }  from 'react'
+import { useFormStatus } from 'react-dom'
+
+const submitUserData = () =>{
+    return new Promise(function (resolve, reject) {
+        setTimeout(resolve, 2000);
+    }).then (function (){
+        return {
+            status: 'success',
+            data: 'User'
+        };
+    });
+};
+
+function SubmitButton (){
+    const  {pending , data } =useFormStatus ();
+
+    return(
+         <>
+         <button type = "submit" disabled={pending}>
+            {pending?  'submitting....': 'submit'}
+         </button>
+         <p>{data ? `Requesting ${data?.get('name')}...` : ''}</p>
+         </>
+    )
+}
+
+export default function Form() {
+  return (
+    <form action={submitUserData} noValidate>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+      </div>
+      <SubmitButton />
+    </form>
+  );
+}
